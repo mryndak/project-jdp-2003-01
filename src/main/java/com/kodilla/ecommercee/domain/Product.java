8ecb278b42d3553eec7711526dd2767e8600d5ec
mturczyn15 <mturczyn15@gmail.com>
2020-03-27 11:34:31 +0100

    JDP200301-13

---
+++ src/main/java/com/kodilla/ecommercee/domain/Product.java
@@ -0,0 +1,8 @@
+package com.kodilla.ecommercee.domain;
+
+public class Product {
+    private Long productId;
+    private String productName;
+    private String description;
+    private int quantity;
+}
