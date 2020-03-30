08b95ba6f908dfeea76f118e18c087c587781b82
jbartek87 <jbartek87@o2.pl>
2020-03-30 09:31:43 +0200

    JDP200301-14

--- src/main/java/com/kodilla/ecommercee/domain/Product.java
+++ src/main/java/com/kodilla/ecommercee/domain/Product.java
@@ -1,8 +1,17 @@
 package com.kodilla.ecommercee.domain;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.math.BigDecimal;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Product {
     private Long productId;
     private String productName;
