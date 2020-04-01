c016dd10ea58b6453b50f29ce78babf2a4a11ed6
Crycald <krzysiekdybikowski@gmail.com>
2020-04-01 17:57:42 +0200

    JDP200301-24 added orderController

--- src/main/java/com/kodilla/ecommercee/OrderController.java
+++ src/main/java/com/kodilla/ecommercee/OrderController.java
@@ -1,4 +1,44 @@
 package com.kodilla.ecommercee;
 
+import com.kodilla.ecommercee.domain.OrderDto;
+import com.kodilla.ecommercee.exceptions.OrderNotFoundException;
+import com.kodilla.ecommercee.mapper.OrderMapper;
+import com.kodilla.ecommercee.service.DbService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/ecommercee/orders")
 public class OrderController {
+    private final OrderMapper orderMapper;
+    private final DbService service;
+
+    @Autowired
+    public OrderController(OrderMapper orderMapper, DbService service) {
+        this.orderMapper = orderMapper;
+        this.service = service;
+    }
+    @RequestMapping(method = RequestMethod.GET, value = "getOrders")
+    public List<OrderDto> getOrders() {
+        return orderMapper.mapToOrderDtoList(service.getAllOrders());
+    }
+    @RequestMapping(method = RequestMethod.GET, value = "getOrder")
+    public OrderDto getOrder(@RequestParam Long orderId) throws OrderNotFoundException {
+        return orderMapper.mapToOrderDto(service.getOrderById(orderId).orElseThrow(OrderNotFoundException::new));
+    }
+    @RequestMapping(method = RequestMethod.DELETE, value = "deleteOrder")
+    public void deleteOrder(@RequestParam Long orderId) {
+        service.deleteOrder(orderId);
+    }
+    @RequestMapping(method = RequestMethod.PUT, value = "updateOrder")
+    public OrderDto updateOrder(@RequestBody OrderDto orderDto) {
+        return orderMapper.mapToOrderDto(service.saveOrder(orderMapper.mapToOrder(orderDto)));
+    }
+    @RequestMapping(method = RequestMethod.POST, value = "createOrder")
+    public void createOrder(@RequestBody OrderDto orderDto) {
+        service.saveOrder(orderMapper.mapToOrder(orderDto));
+    }
 }
