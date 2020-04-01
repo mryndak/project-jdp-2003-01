dfb208827d0683adae05661ea8cd491dbb0f4f82
mturczyn15 <mturczyn15@gmail.com>
2020-04-01 14:16:35 +0200

    refactor

--- src/main/java/com/kodilla/ecommercee/domain/Group.java
+++ src/main/java/com/kodilla/ecommercee/domain/Group.java
@@ -1,9 +1,9 @@
 package com.kodilla.ecommercee.domain;
 
-import lombok.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,7 +21,12 @@ public class Group {
     private String name;
     @Column
     private String description;
-    @Setter
+    @OneToMany(
+            targetEntity = Product.class,
+            mappedBy = "groupId",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY
+    )
     private List<Product> products = new ArrayList<>();
 
     public void addProduct(Product product) {
