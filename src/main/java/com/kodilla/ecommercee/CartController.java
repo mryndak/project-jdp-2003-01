9f3c32675919361e99321c006369195602836544
jbartek87 <jbartek87@o2.pl>
2020-04-01 10:28:00 +0200

    Endpoints for Cart & CartItem

--- src/main/java/com/kodilla/ecommercee/CartController.java
+++ src/main/java/com/kodilla/ecommercee/CartController.java
@@ -1,4 +1,36 @@
 package com.kodilla.ecommercee;
 
+import com.kodilla.ecommercee.domain.CartDto;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+
+@RestController
+@RequestMapping("/v1/ecommercee/cart")
 public class CartController {
+
+
+    @RequestMapping(method = RequestMethod.GET, value = "getCart")
+    public CartDto getCart(Long cartId) {
+        return new CartDto(1L, LocalDate.now());
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "deleteCart")
+    public boolean deleteCart(Long cartId) {
+        System.out.println("Cart has been deleted");
+        return true;
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "updateCart")
+    public CartDto updateProduct(CartDto cartDto) {
+        return new CartDto(1L, LocalDate.of(2020, 10, 10));
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "createCart")
+    public boolean createCart(CartDto cartDto) {
+        System.out.println("Cart was created");
+        return true;
+    }
 }
--- src/main/java/com/kodilla/ecommercee/ProductController.java
+++ src/main/java/com/kodilla/ecommercee/ProductController.java
@@ -1,6 +1,5 @@
 package com.kodilla.ecommercee;
 
-import com.kodilla.ecommercee.domain.Product;
 import com.kodilla.ecommercee.domain.ProductDto;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
