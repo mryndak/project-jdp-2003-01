61dcc0e1781a4a81e45bdaf86ed09ace65a00b12
jbartek87 <jbartek87@o2.pl>
2020-04-04 10:33:59 +0200

    Entity Product with relations

--- src/main/java/com/kodilla/ecommercee/domain/Product.java
+++ src/main/java/com/kodilla/ecommercee/domain/Product.java
@@ -10,18 +10,31 @@ import java.math.BigDecimal;
 @Data
 @NoArgsConstructor
 @Entity
+@Table(name = "PRODUCT")
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long productId;
+
+    @Column(name = "PRODUCT_NAME")
     private String productName;
+
+    @Column(name = "GROUP_ID")
     private Long groupId;
+
+    @Column(name = "DESCRIPTION")
     private String description;
+
+    @Column(name = "QUANTITY")
     private int quantity;
+
+    @Column(name = "PRICE")
     private BigDecimal price;
 
     @ManyToOne
     private CartItem cartItem;
 
+    @ManyToOne
+    private Group group;
 
 }
