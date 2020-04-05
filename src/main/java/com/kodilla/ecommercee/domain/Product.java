b62cb9a9a924c38493a6631c34e2d01f9c5aac61
jbartek87 <jbartek87@o2.pl>
2020-04-05 17:01:24 +0200

    Test of JDP200301-1

---
+++ src/main/java/com/kodilla/ecommercee/domain/Product.java
@@ -0,0 +1,23 @@
+package com.kodilla.ecommercee.domain;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    private Long Id;
+    private String productName;
+    private Long groupId;
+    private String description;
+    private int quantity;
+    private BigDecimal price;
+
+}
