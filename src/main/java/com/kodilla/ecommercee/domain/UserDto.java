f411469e35d8e2485b5972b9907a8d520a176eb7
ijot1 <ireneuszjaneczek128@gmail.com>
2020-04-05 12:47:36 +0200

    test

---
+++ src/main/java/com/kodilla/ecommercee/domain/UserDto.java
@@ -0,0 +1,23 @@
+package com.kodilla.ecommercee.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Builder
+public class UserDto {
+
+    private Long id;
+    private Long orderId;
+    private Long addressId;
+    private String firstName;
+    private String lastName;
+    private String login;
+    private String password;
+    private String email;
+}
