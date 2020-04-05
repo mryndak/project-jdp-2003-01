23cd46c104bd6afb2c5a8c217072290ecf3114e2
Crycald <krzysiekdybikowski@gmail.com>
2020-04-05 17:56:03 +0200

    fix user | userDto | userController

--- src/main/java/com/kodilla/ecommercee/UserController.java
+++ src/main/java/com/kodilla/ecommercee/UserController.java
@@ -22,8 +22,6 @@ public class UserController {
     @RequestMapping(method = RequestMethod.DELETE, value = "deleteUser")
     public void deleteUser(Long userId) {
     }
-
-
     @RequestMapping(method = RequestMethod.PUT, value = "updateUser")
     public UserDto updateUser(Long userId) {
         return new UserDto(1L, 1L, 1L, "STH", "sth", "login", "pwd", "MAIL");
--- src/main/java/com/kodilla/ecommercee/domain/User.java
+++ src/main/java/com/kodilla/ecommercee/domain/User.java
@@ -18,6 +18,4 @@ public class User {
     private String login;
     private String password;
     private String email;
-
-
 }
