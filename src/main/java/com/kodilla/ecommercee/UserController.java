d267a9a5011f051c6155452f37df4cf16e1fa9dc
Crycald <krzysiekdybikowski@gmail.com>
2020-04-05 11:38:18 +0200

    clearly version JDP200301-5

--- src/main/java/com/kodilla/ecommercee/UserController.java
+++ src/main/java/com/kodilla/ecommercee/UserController.java
@@ -1,7 +1,5 @@
 package com.kodilla.ecommercee;
 
-import com.kodilla.ecommercee.domain.OrderStatus;
-import com.kodilla.ecommercee.domain.PaymentType;
 import com.kodilla.ecommercee.domain.UserDto;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -20,8 +18,7 @@ public class UserController {
 
     @RequestMapping(method = RequestMethod.GET, value = "getUser")
     public UserDto getUser(Long userId) {
-        return new UserDto(1L, "test_fname", "test_lname", "test_login",
-                "test_password", "test@email", PaymentType.CREDIT_CARD, OrderStatus.ORDER_SENT);
+        return new UserDto(1L, 1L, 1L, "sth", "sth", "login", "pwd", "mail");
     }
 
     @RequestMapping(method = RequestMethod.DELETE, value = "deleteUser")
@@ -31,9 +28,7 @@ public class UserController {
 
     @RequestMapping(method = RequestMethod.PUT, value = "updateUser")
     public UserDto updateUser(Long userId) {
-        return new UserDto(1L, "edited test_fnam", "edited test_lnam",
-                "edited test_login", "edited test_password", "edited test@email",
-                PaymentType.CREDIT_CARD, OrderStatus.ORDER_SENT);
+        return new UserDto(1L, 1L, 1L, "STH", "sth", "login", "pwd", "MAIL");
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "createUser")
--- src/main/java/com/kodilla/ecommercee/domain/OrderStatus.java
+++
@@ -1,9 +0,0 @@
-package com.kodilla.ecommercee.domain;
-
-public enum OrderStatus {
-    ORDER_PLACED,
-    ORDER_CONFIRMED,
-    PREPARING_ORDER,
-    ORDER_SENT,
-    ORDER_CANCELED
-}
