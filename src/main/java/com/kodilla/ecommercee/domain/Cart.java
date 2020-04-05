f8ceabb27441d6229ea278c0d1fd67ee3cd8fb70
jbartek87 <jbartek87@o2.pl>
2020-04-05 20:59:39 +0200

    Cart & CartItem Entity with Relations

--- src/main/java/com/kodilla/ecommercee/domain/Cart.java
+++ src/main/java/com/kodilla/ecommercee/domain/Cart.java
@@ -1,17 +1,35 @@
 package com.kodilla.ecommercee.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.awt.datatransfer.FlavorEvent;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
-@Getter
-@Setter
+@Data
+@Entity
+@Table(name="CART")
 public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="CART_ID")
     private Long id;
+
+    @NotNull
+    @Column(name="CART_DATE")
     private LocalDate cartDate;
+
+    @OneToMany(
+            targetEntity = CartItem.class,
+            cascade = CascadeType.ALL,
+            mappedBy = "cart",
+            fetch = FetchType.LAZY
+    )
+    private List<CartItem> cartItem = new ArrayList<>();
+
 }
--- src/main/java/com/kodilla/ecommercee/domain/CartItem.java
+++ src/main/java/com/kodilla/ecommercee/domain/CartItem.java
@@ -1,22 +1,39 @@
 package com.kodilla.ecommercee.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 
 @AllArgsConstructor
 @NoArgsConstructor
-@Getter
-@Setter
+@Data
+@Entity
+@Table(name="CART_ITEM")
 public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @Column(name="PRODUCT_ID")
     private Long productId;
+
+    @Column(name="PRODUCT_NAME")
     private String productName;
+
+    @Column(name="PRICE_DATE")
     private LocalDate priceDate;
+
+    @Column(name="PRODUCT_COUNT")
     private Long productCount;
+
+    @Column(name="ADDED_DATE")
     private LocalDate addedDate;
+
+    @ManyToOne
+    @JoinColumn(name="CART_ID")
+    private Cart cart;
+
 }
