792878157621e307266aafa411e7b9a4c56e5943
jbartek87 <jbartek87@o2.pl>
2020-04-06 09:59:23 +0200

    Product Entity with realation v2

--- src/main/java/com/kodilla/ecommercee/domain/Cart.java
+++ src/main/java/com/kodilla/ecommercee/domain/Cart.java
@@ -4,7 +4,6 @@ import lombok.*;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
-import java.awt.datatransfer.FlavorEvent;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
--- src/main/java/com/kodilla/ecommercee/domain/Group.java
+++ src/main/java/com/kodilla/ecommercee/domain/Group.java
@@ -2,13 +2,20 @@ package com.kodilla.ecommercee.domain;
 
 import lombok.*;
 
+import javax.persistence.*;
+
 @AllArgsConstructor
 @NoArgsConstructor
-@Getter
-@Setter
+@Data
 @Builder
+@Entity
+@Table(name="GROUP")
 public class Group {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
     private String groupName;
+
     private String description;
 }
