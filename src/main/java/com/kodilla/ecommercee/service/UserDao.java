0dbbd3d45dce7a12fdfddac3c1bd3e260dfc0f56
Crycald <krzysiekdybikowski@gmail.com>
2020-04-08 11:57:20 +0200

    Renamed DbService -> UserDao + constructor

--- src/main/java/com/kodilla/ecommercee/service/UserDao.java
+++ src/main/java/com/kodilla/ecommercee/service/UserDao.java
@@ -8,9 +8,13 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class DbService {
+public class UserDao {
+    private final UserRepository userRepository;
+
     @Autowired
-    private UserRepository userRepository;
+    public UserDao(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     public List<User> getUsers() {
         return userRepository.findAll();
